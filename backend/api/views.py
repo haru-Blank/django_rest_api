6ece6b1bbfaddf27e2a7cb2227c4b9f61d9dfa7e
haru-blank <avash.nekros274@gmail.com>
2022-10-18 15:51:33 +0545

    read params and responses, simple api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,23 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "This is your django api reponse"})
+    # request ->instance of  HttpRequest -> of Django
+    # request.body
+
+    body = request.body  # byte string of JSON data
+    print(request.body, "body is empty?")
+    data = {}
+    try:
+        data = json.loads(body)  # string of JSON data -> Python Dictionary
+    except:
+        pass
+    # print(body)
+    # print("url params: ", request.GET)  # url query paramss
+    data["headers"] = dict(request.headers)
+    data["content-type"] = request.content_type
+    data["params"] = request.GET
+    # print(data)
+
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,13 +2,23 @@ import requests
 
 endpoint = "https://httpbin.org/status/200"
 endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api"
 
 """ 
 - HTTP Request -> HTML Page
 - REST Api HTTP Request -> json data (or xml or other)
 """
 
-get_response = requests.get(endpoint)  # HTTP Request
-# print(get_response.text)
+# get_response = requests.get(endpoint)  # HTTP Request
+# # print(get_response.text)
 # print(get_response.json())
 # print(get_response.status_code)
+
+
+get_response = requests.get(
+    endpoint, 
+    params={"name": "Djan Djan Djangooo"},
+    json={"query": "Helo World"}
+)
+print(get_response.json())

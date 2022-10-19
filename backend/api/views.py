48eaed39e6bf9f6b506a2f0bfff7ba0887012519
haru-blank <avash.nekros274@gmail.com>
2022-10-19 07:10:35 +0545

    serialize data with model_to_dict method, use api_view decorator with drf Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,14 @@
 import json
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 from products.models import Product
 
 
-def api_home(request, *args, **kwargs):
+""" def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
 
@@ -21,4 +24,18 @@ def api_home(request, *args, **kwargs):
         data["content"] = model_data.content
         data["price"] = model_data.price
 
-    return JsonResponse(data)
+    return JsonResponse(data) """
+
+
+@api_view(["GET"])
+def api_home(request, *args, **kwargs):
+    """
+    DRF Api View
+    """
+    model_data = Product.objects.all().order_by("?").first()
+
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=[ "title", "price"])
+
+    return Response(data)

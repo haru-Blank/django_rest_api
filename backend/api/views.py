e59735c9c14493dbec0c0b0439e67b4ea235d373
haru-blank <avash.nekros274@gmail.com>
2022-10-19 07:04:53 +0545

    serialize our model instance to python dictionary manually

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,24 @@
 import json
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
+
+from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    # request ->instance of  HttpRequest -> of Django
-    # request.body
-
-    body = request.body  # byte string of JSON data
-    print(request.body, "body is empty?")
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)  # string of JSON data -> Python Dictionary
-    except:
-        pass
-    # print(body)
-    # print("url params: ", request.GET)  # url query paramss
-    data["headers"] = dict(request.headers)
-    data["content-type"] = request.content_type
-    data["params"] = request.GET
-    # print(data)
+
+    # serializing our data
+    # taking our model instance
+    # and turning it into a python dictionary
+    # we can read properties of instance
+    # but that's not dicionary
+    # whose property we can read also
+    if model_data:
+        data["id"] = model_data.id
+        data["title"] = model_data.title
+        data["content"] = model_data.content
+        data["price"] = model_data.price
 
     return JsonResponse(data)
